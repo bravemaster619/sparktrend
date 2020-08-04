7a65812fa226cc3094a20eb82199b5ff9ad9ce7b
bravemaster619 <bravemaster619@hotmail.com>
2020-08-04 12:38:27 +0800

    Add args and options to insta cronjob

--- backend/app/Commands/CronjobUpdateInsta.js
+++ backend/app/Commands/CronjobUpdateInsta.js
@@ -5,16 +5,29 @@ const Instaaccount = use("App/Models/Instaaccount")
 
 class UpdateInsta extends Command {
   static get signature () {
-    return 'cronjob:insta'
+    return `
+      cronjob:insta
+      { count?=10 : Number of accounts to update }
+      { username?=\0 : Name of instagram account to update }
+      { --delaccount : Flag to delete unverified account }
+      { --force : Flag to force update regardless of last crawled time }
+    `;
   }
 
   static get description () {
     return "Update instagram account info on a regular basis";
   }
 
-  async handle (args, options) {
-    await this.deleteUnverifiedAccounts()
-    await this.refetchInstagramInfo()
+  async handle(args, options) {
+    if (options.delaccount) {
+      await this.deleteUnverifiedAccounts();
+    }
+    const force = options.force
+    let { username, count } = args
+    if (username === '\u0000') {
+      username = ''
+    }
+    await this.refetchInstagramInfo(username, Number(count), force)
     process.exit()
   }
 
@@ -28,23 +41,27 @@ class UpdateInsta extends Command {
       .delete()
     console.log(`End deleting unverified accounts, deleted ${accounts.result.n} accounts`)
   }
-  async refetchInstagramInfo() {
+  async refetchInstagramInfo(username, count, force) {
     console.log('Begin refetching instagram user info')
     const now = new Date()
     const oldLastCralwedAt = new Date(now.getTime() - 3 * 24 * 3600 * 1000)
+    const where = { verified: true }
+    if (username) {
+      where.username = username
+    }
+    if (!force) {
+      where["$or"] = [
+        {
+          last_crawled_at: null,
+        },
+        {
+          last_crawled_at: { $lte: oldLastCralwedAt },
+        },
+      ];
+    }
     const oldestAccounts = await Instaaccount.query()
-      .where({ verified: true })
-      .where({
-        $or: [
-          {
-            last_crawled_at: null,
-            
-          }, {
-          last_crawled_at: { $lte: oldLastCralwedAt }
-        }
-        ]
-      })
-      .limit(10)
+      .where(where)
+      .limit(count)
       .fetch()
     if (!oldestAccounts.rows || !oldestAccounts.rows.length) {
       console.log('No accounts to update')
